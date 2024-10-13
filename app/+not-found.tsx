85d850c34ac5089a86b98ac25cdb06a39da5e29d
bonekyaw <phonenai2014@gmail.com>
2024-10-13 16:19:22 +0630

    Layout Intro

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import React from 'react';
 
 export default function NotFoundScreen() {
   return (
