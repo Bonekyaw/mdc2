c4261a36b1043d0318438db456a66b747acef80f
bonekyaw <phonenai2014@gmail.com>
2024-10-19 16:30:51 +0630

    json-server setup

---
+++ app/(tabs)/cart.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Cart = () => {
+  return (
+    <SafeAreaView>
+      <Text>cart screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Cart;
+
+const styles = StyleSheet.create({});
---
+++ config/index.ts
@@ -0,0 +1,1 @@
+export const API_URL = "http://192.168.100.50:4000/";
